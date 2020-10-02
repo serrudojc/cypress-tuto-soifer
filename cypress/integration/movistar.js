d52e43f887d4e41c2b6666e1fdaea10abfb03743
serrudojc <serrudo.jc@gmail.com>
2020-10-02 19:14:36 -0300

    Update movistar.js

--- cypress/integration/movistar.js
+++ cypress/integration/movistar.js
@@ -24,6 +24,7 @@ describe("Bateria de casos para Escuelita Cypress 2020", () => {
     cy.get("#myInput").type("A10S").type("{enter}");
     cy.get(".button").click();
     cy.get('.price-installments > [display="inline"]').contains("Hasta 12 cuotas sin interés");
+    cy.get(".details > ul > :nth-child(2)").contains("Hasta 12 cuotas sin interés");
   });
 
   it("Ver que el equipo K40S se pueda pagar en 12 cuotas", () => {
@@ -31,17 +32,27 @@ describe("Bateria de casos para Escuelita Cypress 2020", () => {
     cy.get("#myInput").type("K40S").type("{enter}");
     cy.get(".button").click();
     cy.get('.price-installments > [display="inline"]').contains("Hasta 12 cuotas");
+    cy.get(".details > ul > :nth-child(2)").contains("Hasta 12 cuotas sin interés");
   });
 
-  //es este caso, el texto sólo dice ""
   it("Ver que el equipo A01 se pueda pagar en 12 cuotas", () => {
     cy.get(".waves-effect.dnavigation__search-button.js-desktop-search-button").click();
     cy.get("#myInput").type("A01").type("{enter}");
     cy.get(".button").click();
     cy.get('.price-installments > [display="inline"]').contains("hasta 12 cuotas"); //case sensitive
+    cy.get(".details > ul > :nth-child(2)").contains("Hasta 12 cuotas sin interés");
   });
 
   it("Que se visualice el ordenamiento de mayor a menor", () => {
-    cy.get("#btn-sort>.visible-desk");
+    cy.get("#btn-sort").click();
+    cy.get("#filter-search-bar>.toolbar>.sorter>.btn-group>.dropdown-menu>:nth-child(2)>a>span").click();
+  });
+
+  it("El botón de condiciones legales del A01 debe tener la url correspondiente ", () => {
+    cy.get(".waves-effect.dnavigation__search-button.js-desktop-search-button").click();
+    cy.get("#myInput").type("A01").type("{enter}");
+    cy.get(".button").click();
+    cy.get("#open-modal-installments").click();
+    cy.get("#modal-installments > .modal-dialog > .modal-content > .modal-body > .link-info").should("have.attr", "href", "https://www.movistar.com.ar/legales/promociones/condiciones-financiacion");
   });
 });
