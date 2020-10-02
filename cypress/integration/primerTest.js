75e80fae92daad9eb32046f2e764258d7392eab2
serrudojc <serrudo.jc@gmail.com>
2020-10-02 11:47:37 -0300

    probando

---
+++ cypress/integration/primerTest.js
@@ -0,0 +1,9 @@
+describe("Titulo", function () {
+  var a = 1 + 1;
+  var b = 2;
+  //funcion que contiene todos los casos de prueba a crear
+  it("Descripcion de CP, tiene que quedar claro", function () {
+    //en esta funcion ponemos la prueba en si misma
+    expect(a == b).to.equal(true);
+  });
+});
---
+++ teoria.js
@@ -0,0 +1,32 @@
+/*
+Video 1
+
+Instalacion de Cypress
+npm install --save-dev cypress
+
+
+
+Averiguar luego si se instala en cada proyecto o si luego se puede llamar con algun comando
+O si todo se hace desde aca (las pruebas de diferentes proyectos)
+
+Averiguar el tema de ignore files, que ignorar, que no, compartir pruebas
+
+
+
+desde la ubicacion del proyecto
+.\node_modules\.bin\cypress open
+
+*/
+
+//---------------------------------------------------------------
+/*
+Video 2
+
+en cypress/integration/
+agrego nuestros test
+
+Siempre que quiera hacer Un GRUPO de casos de prueba lo hago con describe
+
+describe('nombre de CP')
+
+*/
